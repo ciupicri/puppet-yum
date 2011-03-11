d02eab850f509760db5ac2b364e8bfc5bea69460
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2011-03-12 01:02:23 +0200

    Update paths

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,22 +7,22 @@ class yumconfig::common {
 class yumconfig::mediarepo {
     include yumconfig::common
 
-    file { "/mnt/misc/${operatingsystem}${operatingsystemrelease}-${architecture}-DVD":
+    file { "/mnt/misc/${operatingsystem}-${operatingsystemrelease}-${architecture}-DVD":
         ensure  => directory,
         require => File["/mnt/misc"],
     }
 
-    mount { "/mnt/misc/${operatingsystem}${operatingsystemrelease}-${architecture}-DVD":
-        device  => "hermes.lizeanunet.tld:/${operatingsystem}${operatingsystemrelease}-${architecture}-DVD",
+    mount { "/mnt/misc/${operatingsystem}-${operatingsystemrelease}-${architecture}-DVD":
+        device  => "hermes.lizeanunet.tld:/${operatingsystem}-${operatingsystemrelease}-${architecture}-DVD",
         fstype  => "nfs",
         options => "ro",
         ensure  => mounted,
-        require => File["/mnt/misc/${operatingsystem}${operatingsystemrelease}-${architecture}-DVD"],
+        require => File["/mnt/misc/${operatingsystem}-${operatingsystemrelease}-${architecture}-DVD"],
     }
 
     yumrepo { "media":
         descr            => 'Fedora $releasever - $basearch media',
-        baseurl          => 'file:///mnt/misc/Fedora$releasever-$basearch-DVD',
+        baseurl          => 'file:///mnt/misc/Fedora-$releasever-$basearch-DVD',
         enabled         => '1',
         gpgcheck        => '0',
         gpgkey          => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-$basearch',
