5f30a00a9955f49ba0042a5b25d0fad20927b105
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2011-03-12 01:37:26 +0200

    Extract yumcache.pp from init.pp

---
+++ manifests/yumcache.pp
@@ -0,0 +1,28 @@
+class yumconfig::yumcache {
+    include yumconfig::common
+
+    mount { "/var/cache/yum":
+        device  => "hermes.lizeanunet.tld:/${operatingsystem}-yum-cache",
+        fstype  => "nfs",
+        options => "rw",
+        ensure  => mounted,
+    }
+
+    file { "/mnt/misc/yum-packages":
+        ensure  => directory,
+        require => File["/mnt/misc"],
+    }
+
+    mount { "/mnt/misc/yum-packages":
+        device  => "hermes.lizeanunet.tld:/${operatingsystem}-yum-packages/${operatingsystemrelease}",
+        fstype  => "nfs",
+        options => "rw",
+        ensure  => mounted,
+        require => File["/mnt/misc/yum-packages"],
+    }
+
+    augeas { "yum":
+        context => "/files/etc/yum.conf/main",
+        changes => "set keepcache 1",
+    }
+}
