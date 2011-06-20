b837fccb6509c069ca4680bd6dad46cc433711c3
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2011-06-20 19:17:36 +0300

    Add EPEL repository

---
+++ manifests/repos/epel.pp
@@ -0,0 +1,16 @@
+class yum::repos::epel {
+    yumrepo { 'epel':
+        descr       => 'Extra Packages for Enterprise Linux 5 - $basearch',
+        mirrorlist  => 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-5&arch=$basearch',
+        enabled     => '1',
+        gpgcheck    => '1',
+        gpgkey      => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL',
+    }
+
+    file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL':
+        owner  => "root",
+        group  => "root",
+        mode   => "0644",
+        source => "puppet:///modules/${caller_module_name}/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL",
+    }
+}
