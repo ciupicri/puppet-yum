4e837017076229b38a09e2024abba323b6a4dc36
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2011-07-06 19:10:54 +0300

    WIP anchors http://projects.puppetlabs.com/issues/8040

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,36 @@
 class yum {
-    include repos::media
-    include repos::tmz-puppet
-    include repos::ciupicri
-    include cache
+    anchor { 'yum::begin': }
+    anchor { 'yum::end': }
+
+    class { 'repos::media':
+        require => Anchor['yum::begin'],
+        before  => Anchor['yum::end'],
+    }
+    class { 'repos::tmz-puppet':
+        require => Anchor['yum::begin'],
+        before  => Anchor['yum::end'],
+    }
+    class { 'repos::ciupicri':
+        require => Anchor['yum::begin'],
+        before  => Anchor['yum::end'],
+    }
+    class { 'cache':
+        require => Anchor['yum::begin'],
+        before  => Anchor['yum::end'],
+    }
 
     case $operatingsystem {
-        "CentOS": { include repos::epel5 }
-        "Scientific": { include repos::epel6 }
+        "CentOS": {
+            class { 'repos::epel5':
+                require => Anchor['yum::begin'],
+                before  => Anchor['yum::end'],
+            }
+        }
+        "Scientific": {
+            class { 'repos::epel6':
+                require => Anchor['yum::begin'],
+                before  => Anchor['yum::end'],
+            }
+        }
     }
 }
